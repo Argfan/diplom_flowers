532d4f7a7d9b58b021b9dd7d14d898bab7ade385
argfan <argfan2@gmail.com>
2024-05-17 19:12:32 +0600

    1

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,7 @@
 import { ref } from 'vue';
 import HelloWorld from './components/HelloWorld.vue'
 
-const date = ref('')
 
-const selectedCity = ref();
-const cities = ref([
-    { name: 'New York', code: 'NY' },
-    { name: 'Rome', code: 'RM' },
-    { name: 'London', code: 'LDN' },
-    { name: 'Istanbul', code: 'IST' },
-    { name: 'Paris', code: 'PRS' }
-]);
 
 
 </script>
@@ -19,14 +10,10 @@ const cities = ref([
 
 
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  <Calendar v-model="date" />
+  <div class="flex h-screen overflow-hidden">
 
-  <div class="card flex justify-center">
-        <Dropdown v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select a City" class="w-full md:w-[14rem]" />
-    </div>
+    
+  </div>
 </template>
 
 <style scoped>
